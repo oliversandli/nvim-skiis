68923e8b38577db9ea0852f9d189f2e6e03c9653
oliversandli <oliversandli@icloud.com>
2020-04-27 13:41:36 -0500

    Fix skiis.py

--- skiis.py
+++ skiis.py
@@ -11,9 +11,13 @@ class Skiis(object):
     @pynvim.command("SkiiRun", sync=True)
     def exec_conf(self):
         """exec_conf"""
+        pwd = self.nvim.command_output("pwd")
         try:
-            with open(".skiiconf", "r") as f:
-                conf_file = f.read()
+            with open(f"{pwd}/.skiiconf", "r") as f:
+                conf_contents = f.read()
         except FileNotFoundError:
+            self.nvim.out_write("ERROR: could not find .skiiconf")
             return
-        self.nvim.funcs.execute(f"!{conf_file[:-1]}")
+        self.nvim.command("set splitright")
+        self.nvim.command(f"vsplit | terminal {conf_contents}")
+        self.nvim.command("startinsert")
