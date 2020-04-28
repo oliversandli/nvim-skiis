a7ac5a07d1f92d200a6a5e7cf5ed8b100d5e8976
oliversandli <oliversandli@icloud.com>
2020-04-28 10:51:21 -0500

    Improve PEP and docstrings

--- skiis.py
+++ skiis.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
-"""skiis v0.01"""
+"""skiis v1.0.0"""
 
 import pynvim
 
 @pynvim.plugin
 class Skiis(object):
+    """Skiis"""
     def __init__(self, nvim):
         self.nvim = nvim
 
     @pynvim.command("SkiiRun", sync=True)
     def exec_conf(self):
-        """exec_conf"""
+        """run the command in conf_contents"""
         pwd = self.nvim.command_output("pwd")
         try:
-            with open(f"{pwd}/.skiiconf", "r") as f:
-                conf_contents = f.read()
+            with open(f"{pwd}/.skiiconf", "r") as open_file:
+                conf_contents = open_file.read()
         except FileNotFoundError:
             self.nvim.out_write("ERROR: could not find .skiiconf")
             return
